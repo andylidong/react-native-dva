8dd2d7ddad5209afd69dd95d7317549d6ac8987a
andylidong <andy_lidong@foxmail.com>
2019-12-11 16:16:47 +0800

    update jest

---
+++ __tests__/utils.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+import { sum } from '../src/utils';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 2 + 2 to greaterThanOrEqual 3', () => {
+  expect(sum(2, 2)).toBeGreaterThanOrEqual(3);
+});
+
+test('two plus two', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  // toBe and toEqual are equivalent for numbers
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+});
